57e06fce2f116cb14c68675de319472448eb4580
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-30 00:30:31 +0100

    Implement UI functionality

--- unicafe-redux/src/main.jsx
+++ unicafe-redux/src/main.jsx
@@ -6,21 +6,23 @@ import reducer from "./reducer";
 const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: "GOOD",
-    });
-  };
+  const handleGood = () => store.dispatch({ type: "GOOD" });
+  const handleOk = () => store.dispatch({ type: "OK" });
+  const handleBad = () => store.dispatch({ type: "BAD" });
+  const handleReset = () => store.dispatch({ type: "ZERO" });
+  const good = store.getState().good;
+  const ok = store.getState().ok;
+  const bad = store.getState().bad;
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleOk}>ok</button>
+      <button onClick={handleBad}>bad</button>
+      <button onClick={handleReset}>reset stats</button>
+      <div>good {good}</div>
+      <div>ok {ok}</div>
+      <div>bad {bad}</div>
     </div>
   );
 };
