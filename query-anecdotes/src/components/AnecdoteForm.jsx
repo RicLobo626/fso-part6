5dccf38635ce4576272101adc0f3ceab39c40203
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-02 01:37:55 +0100

    Add functionality for creating anecdotes with React Query

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,34 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote } from "../services/anecdotesService";
+import { useRef } from "react";
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+const AnecdoteForm = () => {
+  const formRef = useRef();
+  const queryClient = useQueryClient();
+
+  const createMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+      formRef.current.reset();
+    },
+  });
+
+  const handleCreate = (e) => {
+    e.preventDefault();
+    const content = e.target.anecdote.value;
+    createMutation.mutate({ content, votes: 0 });
+  };
 
   return (
     <div>
       <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
+      <form onSubmit={handleCreate} ref={formRef}>
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
